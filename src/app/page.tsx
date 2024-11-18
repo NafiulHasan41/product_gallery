aa8250827f0cd949424ddf08344263ba21329b72
NafiulHasan41 <nhashik40@gmail.com>
2024-11-18 16:18:39 +0600

    Installed nextui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-
+import {Button} from '@nextui-org/button'; 
 
 export default function Home() {
   return (
     <div>
+            <Button>Click me</Button>
       <h1 className=" text-red-500 text-3xl">Hello</h1>
     </div>
   );
